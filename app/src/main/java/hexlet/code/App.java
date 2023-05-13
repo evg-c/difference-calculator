3c5785dd28805045a9f37956073d87e7717fb624
Evgeny_Ch <gek_sh@mail.ru>
2023-05-13 21:49:10 +0300

    Added picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,22 @@
+/**
+ * Picocli: gendiff
+ * Specify another file name as command line parameter, e.g. '/usr/bin/java' or '/proc/cpuinfo'
+ * Specify another file name as command line parameter, e.g. '/usr/bin/java' or '/proc/cpuinfo'
+ * @author Evgeny_ch
+ */
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
 public class App {
     public static void main(String[] args) {
+        new CommandLine(new App()).execute(args);
         System.out.println("Hello World!");
+        //System.exit(exitCode);
     }
 }
