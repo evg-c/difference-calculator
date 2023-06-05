0b7bfc33190bfd1ad8e90c34ff98c6487b66de34
Evgeny_Ch <gek_sh@mail.ru>
2023-06-06 00:50:22 +0300

    Added App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,8 @@
 /**
  * Picocli: gendiff
- * Specify another file name as command line parameter, e.g. '/usr/bin/java' or '/proc/cpuinfo'
- * Specify another file name as command line parameter, e.g. '/usr/bin/java' or '/proc/cpuinfo'
+ * Specify another file name as command line parameter, e.g. '/src/test/resources/file1.json'
+ * Specify another file name as command line parameter, e.g. '/src/test/resources/file2.json'
+ * Specify format as command line parameter, e.g. '-f plain'
  * @author Evgeny_ch
  */
 package hexlet.code;
@@ -11,19 +12,45 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 import java.io.File;
+import java.util.concurrent.Callable;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import static hexlet.code.Differ.generate;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
-public class App {
-    @Parameters(index = "0", description = "path to first file")
-    private File filepath1;
-    @Parameters(index = "1", description = "path to second file")
-    private File filepath2;
-    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+public class App implements Callable<Integer> {
+
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
     private String format = "stylish";
-    public static void main(String[] args) {
-        new CommandLine(new App()).execute(args);
-        System.out.println("Hello World!");
-        //System.exit(exitCode);
+//    @Option(names = {"-V", "--version"}, versionHelp = true, description = "display version info")
+//    boolean versionInfoRequested;
+//    @Option(names = {"-h", "--help"}, usageHelp = true, description = "display this help message")
+//    boolean usageHelpRequested;
+    private static final int SUCCESS_EXIT_CODE = 0;
+    private static final int ERROR_EXIT_CODE = 1;
+
+    @Override
+    public Integer call() throws Exception { // your business logic goes here...
+        try {
+            String formattedDiff = generate(filepath1, filepath2, format);
+            System.out.println(formattedDiff);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return ERROR_EXIT_CODE;
+        }
+        return SUCCESS_EXIT_CODE;
     }
+
+    public static void main(String[] args) throws Exception {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
 }
