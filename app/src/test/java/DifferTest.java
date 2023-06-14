8324de379e77dd03c3d6030024ba8d356c4e7219
Evgeny_Ch <gek_sh@mail.ru>
2023-06-15 00:58:56 +0300

    Added test for yml

--- app/src/test/java/DifferTest.java
+++ app/src/test/java/DifferTest.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class DifferTest {
 
     @Test
-    public void testGenerateDiff() throws IOException {
+    public void testGenerateDiffFromJson() throws IOException {
         StringBuilder str = new StringBuilder();
         str.append("{\n");
         str.append("  - follow: false\n");
@@ -18,6 +18,21 @@ public class DifferTest {
         str.append("  + verbose: true\n");
         str.append("}");
         String diffFromApp = Differ.generate("src/test/resources/file1.json", "src/test/resources/file2.json");
+        assertEquals(str.toString(), diffFromApp);
+    }
+
+    @Test
+    public void testGenerateDiffFromYml() throws IOException {
+        StringBuilder str = new StringBuilder();
+        str.append("{\n");
+        str.append("  - follow: false\n");
+        str.append("    host: hexlet.io\n");
+        str.append("  - proxy: 123.234.53.22\n");
+        str.append("  - timeout: 50\n");
+        str.append("  + timeout: 20\n");
+        str.append("  + verbose: true\n");
+        str.append("}");
+        String diffFromApp = Differ.generate("src/test/resources/file1.yml", "src/test/resources/file2.yml");
         assertEquals(str.toString(), diffFromApp);
     }
 }
