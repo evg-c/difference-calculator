e2b581f847a929ee96b5d59d13cd842e4a6771a1
Evgeny_Ch <gek_sh@mail.ru>
2023-06-15 00:58:17 +0300

    Added for yaml

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,6 +1,7 @@
 package hexlet.code;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 import java.io.File;
 import java.io.IOException;
@@ -17,39 +18,33 @@ public class Differ {
     }
 
     public static String generate(String filepath1, String filepath2, String format) throws IOException {
-        // сначала проверим параметры как файлы по отдельности (что они существуют и доступны для чтения)
-        if (!checkParameterFile(filepath1) || (!checkParameterFile(filepath2))) {
-            return "";
-        }
-        // теперь проверим, что сравниваем разные файлы
-        if (filepath1.equals(filepath2)) {
-            System.out.println("В обоих параметрах один и тот же файл!");
+        if (!checkingParameters(filepath1, filepath2)) {
             return "";
         }
         // формируем мапы из входных json-файлов
-        Map<String, Object> mapFromJson1 = readJsonFile(filepath1);
-        Map<String, Object> mapFromJson2 = readJsonFile(filepath2);
+        Map<String, Object> mapFromFile1 = Parser.readFileToMap(filepath1);
+        Map<String, Object> mapFromFile2 = Parser.readFileToMap(filepath2);
         // формируем общий список ключей
-        TreeSet<String> keys = new TreeSet<>(mapFromJson1.keySet());
-        keys.addAll(mapFromJson2.keySet());
+        TreeSet<String> keys = new TreeSet<>(mapFromFile1.keySet());
+        keys.addAll(mapFromFile2.keySet());
         // формируем мапу различий
         Map<String, Object> resultMapDiff = new LinkedHashMap<>();
         for (String key: keys) {
             Map<String, Object> oneRecord = new LinkedHashMap<>();
-            oneRecord = generateDiff(key, mapFromJson1, mapFromJson2);
+            oneRecord = generateDiff(key, mapFromFile1, mapFromFile2);
             resultMapDiff.putAll(oneRecord);
         }
         String resultString = toString(resultMapDiff);
         return resultString;
     }
 
-    public static boolean checkParameterFile(String pathfile) throws IOException {
+    public static boolean checkParameterFile(String file) throws IOException {
         // проверка файла, переданного в параметре
-        if (pathfile.isEmpty()) {
+        if (file.isEmpty()) {
             System.out.println("Путь к файлу пуст");
             return false;
         }
-        Path pathForFile = Paths.get(pathfile);
+        Path pathForFile = Paths.get(file);
         if (!Files.exists(pathForFile)) {
             System.out.println("Файл не существует");
             return false;
@@ -64,14 +59,23 @@ public class Differ {
         }
         return true;
     }
-
-    public static Map<String, Object> readJsonFile(String filepath) throws IOException {
-        // читаем файл и формируем из его данных структуру Map
-        Path pathForFile = Paths.get(filepath);
-        File inFile = pathForFile.toFile();
-        ObjectMapper mapperJson = new ObjectMapper();
-        return mapperJson.readValue(inFile, Map.class);
-    }
+//
+//    public static Map<String, Object> readFileToMap(String filepath) throws IOException {
+//        // читаем файл и формируем из его данных структуру Map
+//        Path pathForFile = Paths.get(filepath);
+//        File inFile = pathForFile.toFile();
+//        String extensionFile = getExtensionFile(filepath);
+//        switch (extensionFile) {
+//            case ("json"), ("JSON"):
+//                ObjectMapper mapperJson = new ObjectMapper();
+//                return mapperJson.readValue(inFile, Map.class);
+//            case ("yml"), ("YML"):
+//                ObjectMapper mapperYml = new YAMLMapper();
+//                return mapperYml.readValue(inFile, Map.class);
+//            default:
+//                throw new IllegalStateException("Unexpected value: " + extensionFile);
+//        }
+//    }
 
     public static Map<String, Object> generateDiff(String keyMap, Map<String, Object> map1, Map<String, Object> map2) {
         // генерируем одну запись различий для конкретного ключа, передаваемого в качестве параметра
@@ -129,5 +133,35 @@ public class Differ {
         }
         rezult.append("}");
         return rezult.toString();
+    }
+
+    public static String getExtensionFile(String pathFile) {
+        Path pathForFile = Paths.get(pathFile);
+        File inFile = pathForFile.toFile();
+        String nameFile = inFile.getName();
+        int i = nameFile.lastIndexOf(".");
+        String extension = i > 0 ? nameFile.substring(i + 1) : "";
+        return extension;
+    }
+
+    public static boolean checkingParameters (String file1, String file2) throws IOException {
+        // сначала проверим параметры как файлы по отдельности (что они существуют и доступны для чтения)
+        if (!checkParameterFile(file1) || (!checkParameterFile(file2))) {
+            return false;
+        }
+        // проверим, что сравниваем разные файлы
+        if (file1.equals(file2)) {
+            System.out.println("В обоих параметрах один и тот же файл!");
+            return false;
+        }
+        // узнать расширение файла
+        String extensionFile1 = getExtensionFile(file1);
+        String extensionFile2 = getExtensionFile(file2);
+        // проверим, что файлы имеют одинаковое расширение
+        if (!extensionFile1.equals(extensionFile2)) {
+            System.out.println("Файлы с разными расширениями (разного типа) !");
+            return false;
+        }
+        return true;
     }
 }
