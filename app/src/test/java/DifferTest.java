71a18d3f1f9d44e31acd9f35b35a60a32e9aa47f
Evgeny_Ch <gek_sh@mail.ru>
2023-06-17 00:13:11 +0300

    Added test for json nested and yml nested

--- app/src/test/java/DifferTest.java
+++ app/src/test/java/DifferTest.java
@@ -3,11 +3,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import hexlet.code.Differ;
 import org.junit.jupiter.api.Test;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class DifferTest {
 
     @Test
-    public void testGenerateDiffFromJson() throws IOException {
+    public void testGenerateDiffFromJsonOld() throws IOException {
         StringBuilder str = new StringBuilder();
         str.append("{\n");
         str.append("  - follow: false\n");
@@ -22,7 +24,14 @@ public class DifferTest {
     }
 
     @Test
-    public void testGenerateDiffFromYml() throws IOException {
+    public void testGenerateDiffFromJsonNew() throws IOException {
+        String resultDiff;
+        resultDiff = Files.readString(Path.of("src/test/resources/result1.txt"));
+        String diffFromApp = Differ.generate("src/test/resources/file1.json", "src/test/resources/file2.json");
+        assertEquals(resultDiff, diffFromApp);
+    }
+    @Test
+    public void testGenerateDiffFromYmlOld() throws IOException {
         StringBuilder str = new StringBuilder();
         str.append("{\n");
         str.append("  - follow: false\n");
@@ -34,5 +43,29 @@ public class DifferTest {
         str.append("}");
         String diffFromApp = Differ.generate("src/test/resources/file1.yml", "src/test/resources/file2.yml");
         assertEquals(str.toString(), diffFromApp);
+    }
+
+    @Test
+    public void testGenerateDiffFromYmlNew() throws IOException {
+        String resultDiff;
+        resultDiff = Files.readString(Path.of("src/test/resources/result1.txt"));
+        String diffFromApp = Differ.generate("src/test/resources/file1.yml", "src/test/resources/file2.yml");
+        assertEquals(resultDiff, diffFromApp);
+    }
+
+    @Test
+    public void testGenerateDiffFromJsonNested() throws IOException {
+        String correctDiff;
+        correctDiff = Files.readString(Path.of("src/test/resources/result2.txt"));
+        String diffFromApp = Differ.generate("src/test/resources/file3.json", "src/test/resources/file4.json");
+        assertEquals(correctDiff, diffFromApp);
+    }
+
+    @Test
+    public void testGenerateDiffFromYmlNested() throws IOException {
+        String correctDiff;
+        correctDiff = Files.readString(Path.of("src/test/resources/result2.txt"));
+        String diffFromApp = Differ.generate("src/test/resources/file5.yml", "src/test/resources/file6.yml");
+        assertEquals(correctDiff, diffFromApp);
     }
 }
