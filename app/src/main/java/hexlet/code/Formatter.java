597dd8a355e043901a6890e44366e5fc39928575
Evgeny_Ch <gek_sh@mail.ru>
2023-06-27 01:10:24 +0300

    Added json format

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -1,17 +1,25 @@
 package hexlet.code;
 
+import hexlet.code.formatters.FormatJson;
 import hexlet.code.formatters.FormatPlain;
 import hexlet.code.formatters.FormatStylish;
+
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 public class Formatter {
-    public static String formatting(String format, List<ElementOfDiff> resultList) {
+//    public static String formatting(String format, List<ElementOfDiff> resultList) {
+    public static String formatting(String format, List<Map<String, Object>> resultList) throws IOException {
         switch (format) {
             case "stylish" -> {
                 return FormatStylish.formattingStylish(resultList);
             }
             case "plain" -> {
                 return FormatPlain.formattingPlain(resultList);
+            }
+            case "json", "JSON" -> {
+                return FormatJson.formattingJson(resultList);
             }
             default -> {
                 throw new IllegalStateException("Unexpected format: " + format);
