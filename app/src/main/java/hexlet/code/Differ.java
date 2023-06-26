c97aef14d38a57785e505f7e473186a936ce6c79
Evgeny_Ch <gek_sh@mail.ru>
2023-06-27 01:09:35 +0300

    Refactoring to new structure

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -5,10 +5,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeSet;
-import java.util.List;
-import java.util.ArrayList;
 
 public class Differ {
     public static String generate(String filepath1, String filepath2) throws IOException {
@@ -26,9 +27,14 @@ public class Differ {
         TreeSet<String> keys = new TreeSet<>(mapFromFile1.keySet());
         keys.addAll(mapFromFile2.keySet());
         // формируем мапу различий
-        List<ElementOfDiff> resultListDiff = new ArrayList<>();
+//        List<ElementOfDiff> resultListDiff = new ArrayList<>();
+//        for (String key: keys) {
+//            ElementOfDiff oneRecord = (ElementOfDiff) generateDiff(key, mapFromFile1, mapFromFile2);
+//            resultListDiff.add(oneRecord);
+//        }
+        List<Map<String, Object>> resultListDiff = new ArrayList<>();
         for (String key: keys) {
-            ElementOfDiff oneRecord = (ElementOfDiff) generateDiff(key, mapFromFile1, mapFromFile2);
+            Map<String, Object> oneRecord = generateDiff(key, mapFromFile1, mapFromFile2);
             resultListDiff.add(oneRecord);
         }
         String resultString = Formatter.formatting(format, resultListDiff);
@@ -57,30 +63,55 @@ public class Differ {
         return true;
     }
 
-    public static ElementOfDiff generateDiff(String keyMap, Map<String, Object> map1, Map<String, Object> map2) {
+//    public static ElementOfDiff generateDiff(String keyMap, Map<String, Object> map1, Map<String, Object> map2) {
+//        // генерируем одну запись различий для конкретного ключа, передаваемого в качестве параметра (или две записи)
+//        String statusElement = "unchanged";
+//        Object oldValue;
+//        Object newValue;
+//        if (map1.keySet().contains(keyMap) && (!map2.keySet().contains(keyMap))) {
+//            // есть в первом файле и нет во втором, то есть ключ удален: -
+//            statusElement = "deleted";
+//            oldValue = map1.get(keyMap);
+//        }
+//        if (!map1.keySet().contains(keyMap) && (map2.keySet().contains(keyMap))) {
+//            // нет в первом файле, есть во втором? то есть ключ добавлен: +
+//            statusElement = "added";
+//            newValue = map2.get(keyMap);
+//        }
+//        if (map1.keySet().contains(keyMap) && (map2.keySet().contains(keyMap))) {
+//            // ключ есть и в первом, и во втором файле, надо сравнивать значения
+//            return genDiffTwoFiles(keyMap, map1, map2);
+//        }
+//        ElementOfDiff recordDiff = new ElementOfDiff(keyMap, statusElement, oldValue, newValue);
+//        return recordDiff;
+//    }
+
+    public static Map<String, Object> generateDiff(String keyMap, Map<String, Object> map1, Map<String, Object> map2) {
         // генерируем одну запись различий для конкретного ключа, передаваемого в качестве параметра (или две записи)
         String statusElement = "unchanged";
+        Object oldValue = null;
+        Object newValue = null;
         if (map1.keySet().contains(keyMap) && (!map2.keySet().contains(keyMap))) {
             // есть в первом файле и нет во втором, то есть ключ удален: -
             statusElement = "deleted";
+            oldValue = map1.get(keyMap);
         }
         if (!map1.keySet().contains(keyMap) && (map2.keySet().contains(keyMap))) {
             // нет в первом файле, есть во втором? то есть ключ добавлен: +
             statusElement = "added";
+            newValue = map2.get(keyMap);
         }
         if (map1.keySet().contains(keyMap) && (map2.keySet().contains(keyMap))) {
             // ключ есть и в первом, и во втором файле, надо сравнивать значения
-            return genDiffTwoFiles(keyMap, map1, map2);
+            return diffTwo(keyMap, map1, map2);
         }
-        ElementOfDiff recordDiff = new ElementOfDiff(keyMap, statusElement, map1.get(keyMap), map2.get(keyMap));
-        return recordDiff;
+        return elementDiff(keyMap, statusElement, oldValue, newValue);
     }
 
-    public static ElementOfDiff genDiffTwoFiles(String keyMap, Map<String, Object> map1, Map<String, Object> map2) {
+    public static Map<String, Object> diffTwo(String keyMap, Map<String, Object> map1, Map<String, Object> map2) {
+        // ключ есть и в первом, и во втором файле, надо сравнивать значения
         String statusElement = "unchanged";
         Object valueMap = map1.get(keyMap);
-        // ключ есть и в первом, и во втором файле, надо сравнивать значения
-        valueMap = map1.get(keyMap);
         Object valueMap2 = map2.get(keyMap);
         // необходимо проверить значения обоих ключей на null
         if ((valueMap == null) || (valueMap2 == null) || (!valueMap.equals(valueMap2))) {
@@ -88,10 +119,19 @@ public class Differ {
         } else {
             statusElement = "unchanged";
         }
-        ElementOfDiff recordDiff = new ElementOfDiff(keyMap, statusElement, map1.get(keyMap), map2.get(keyMap));
-        return recordDiff;
+//        ElementOfDiff recordDiff = new ElementOfDiff(keyMap, statusElement, valueMap, valueMap2);
+        return elementDiff(keyMap, statusElement, valueMap, valueMap2);
     }
 
+    public static Map<String, Object> elementDiff(String keyMap, String status, Object oldElement, Object newElement) {
+        LinkedHashMap recordDiff = new LinkedHashMap();
+        recordDiff.put("statusDiff", status);
+        recordDiff.put("oldValue", oldElement);
+        recordDiff.put("newValue", newElement);
+        LinkedHashMap recordList = new LinkedHashMap();
+        recordList.put(keyMap, recordDiff);
+        return recordList;
+    }
     public static String getExtensionFile(String pathFile) {
         Path pathForFile = Paths.get(pathFile);
         File inFile = pathForFile.toFile();
