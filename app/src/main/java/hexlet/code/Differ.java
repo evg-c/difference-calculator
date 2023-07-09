a52bdb0cc192d81b6a18098860eac32060982b8e
Evgeny_Ch <gek_sh@mail.ru>
2023-07-10 01:27:54 +0300

    Refactoring

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -5,10 +5,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeSet;
 
 public class Differ {
     public static String generate(String filepath1, String filepath2) throws IOException {
@@ -16,51 +14,46 @@ public class Differ {
     }
 
     public static String generate(String filepath1, String filepath2, String format) throws IOException {
-        if (!checkingParameters(filepath1, filepath2)) {
+        if (!checkingParameters(filepath1, filepath2, format)) {
             return "";
         }
         String string1 = Files.readString(Path.of(filepath1));
         String string2 = Files.readString(Path.of(filepath2));
         String extension1 = Differ.getExtensionFile(filepath1);
         String extension2 = Differ.getExtensionFile(filepath2);
-        // формируем мапы из входных json-файлов
-//        Map<String, Object> map1 = Parser.readFileToMap(filepath1);
-//        Map<String, Object> map2 = Parser.readFileToMap(filepath2);
         Map<String, Object> map1 = Parser.readStringToMap(string1, extension1);
         Map<String, Object> map2 = Parser.readStringToMap(string2, extension2);
-        // формируем общий список ключей
-        TreeSet<String> keys = new TreeSet<>(map1.keySet());
-        keys.addAll(map2.keySet());
-        // формируем мапу различий
-        List<Map<String, Object>> resultListDiff = new ArrayList<>();
-        for (String key: keys) {
-            Map<String, Object> oneRecord = CreateDataMap.generateDiff(key, map1, map2);
-            resultListDiff.add(oneRecord);
-        }
+        List<Map<String, Object>> resultListDiff;
+        resultListDiff = CreateDataMap.generateListDiff(map1, map2);
         String resultString = Formatter.formatting(format, resultListDiff);
         return resultString;
     }
 
     public static boolean checkParameterFile(String file) throws IOException {
-        // проверка файла, переданного в параметре
+        boolean resultCheck = true;
         if (file.isEmpty()) {
-            System.out.println("Путь к файлу пуст");
-            return false;
+            System.out.println("Путь к файлу " + file + " не указан");
+            resultCheck = false;
         }
         Path pathForFile = Paths.get(file);
-        if (!Files.exists(pathForFile)) {
-            System.out.println("Файл не существует");
-            return false;
+        if (resultCheck && !Files.exists(pathForFile)) {
+            System.out.println("Файл " + file + " не существует");
+            resultCheck = false;
         }
-        if (!Files.isReadable(pathForFile)) {
-            System.out.println("Файл недоступен для чтения");
-            return false;
+        if (resultCheck && !Files.isReadable(pathForFile)) {
+            System.out.println("Файл " + file + " недоступен для чтения");
+            resultCheck = false;
         }
-        if (Files.isDirectory(pathForFile)) {
-            System.out.println("Это каталог, а не файл!");
-            return false;
+        if (resultCheck && Files.isDirectory(pathForFile)) {
+            System.out.println(file + " это каталог, а не файл!");
+            resultCheck = false;
         }
-        return true;
+        String contentFile = resultCheck ? Files.readString(pathForFile) : "";
+        if (resultCheck && contentFile.isEmpty()) {
+            System.out.println("Содержимое файла " + file + " пустое!");
+            resultCheck = false;
+        }
+        return resultCheck;
     }
 
     public static String getExtensionFile(String pathFile) {
@@ -72,34 +65,25 @@ public class Differ {
         return extension;
     }
 
-    public static boolean checkingParameters(String file1, String file2) throws IOException {
-        // сначала проверим параметры как файлы по отдельности (что они существуют и доступны для чтения)
+    public static boolean checkingParameters(String file1, String file2, String format) throws IOException {
+        boolean resultCheck = true;
         if (!checkParameterFile(file1) || (!checkParameterFile(file2))) {
-            return false;
+            resultCheck = false;
         }
-        // проверим, что сравниваем разные файлы
-        if (file1.equals(file2)) {
+        if (resultCheck && file1.equals(file2)) {
             System.out.println("В обоих параметрах один и тот же файл!");
-            return false;
+            resultCheck = false;
         }
-        // узнать расширение файла
-        String extensionFile1 = getExtensionFile(file1);
-        String extensionFile2 = getExtensionFile(file2);
-        // проверим, что файлы имеют одинаковое расширение
-        if (!extensionFile1.equals(extensionFile2)) {
+        String extensionFile1 = resultCheck ? getExtensionFile(file1) : "";
+        String extensionFile2 = resultCheck ? getExtensionFile(file2) : "";
+        if (resultCheck && !extensionFile1.equals(extensionFile2)) {
             System.out.println("Файлы с разными расширениями (разного типа) !");
-            return false;
+            resultCheck = false;
         }
-        String string1 = Files.readString(Path.of(file1));
-        String string2 = Files.readString(Path.of(file2));
-        if (string1.isEmpty()) {
-            System.out.println("Файл" + file1 + " пустой!");
-            return false;
+        if (resultCheck && (!format.equals("stylish") && !format.equals("plain") && !format.equals("json"))) {
+            System.out.println("Неизвестное значение параметра формата: " + format);
+            resultCheck = false;
         }
-        if (string2.isEmpty()) {
-            System.out.println("Файл" + file2 + " пустой!");
-            return false;
-        }
-        return true;
+        return resultCheck;
     }
 }
